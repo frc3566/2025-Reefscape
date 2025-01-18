d517851110dcaa5e7e0b2fc8f27d80b84a2933f7
GregoryLi360 <gregoryli360@gmail.com>
2025-01-18 11:33:08 -0500

    fix: CTREConfig

--- src/main/java/frc/lib/CTREConfigs.java
+++ src/main/java/frc/lib/CTREConfigs.java
@@ -2,8 +2,7 @@ package frc.lib;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.MagnetSensorConfigs;
-import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue; //deprecated to AbsoluteSensorDiscontinuityPoint
-// import com.ctre.phoenix.sensors.SensorTimeBase;
+import com.ctre.phoenix.sensors.SensorTimeBase;
 
 import frc.robot.Constants;
 
@@ -14,8 +13,8 @@ public final class CTREConfigs {
     swerveCanCoderConfig = new CANcoderConfiguration()
         .withMagnetSensor(
             new MagnetSensorConfigs()
-                .withAbsoluteSensorRange(AbsoluteSensorRangeValue.Unsigned_0To1) //replace with discontinuity point
-                .withSensorDirection(Constants.Swerve.canCoderInvert)
+              .withAbsoluteSensorDiscontinuityPoint(1)
+              .withSensorDirection(Constants.Swerve.canCoderInvert)
         );
 
     // swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond; TODO: find deprecation replacement
