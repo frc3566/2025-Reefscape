da26fbcb6e6780ee77affbe62abde6408d0de14f
kaaaien <kaienhung999@gmail.com>
2025-02-26 16:18:48 -0500

    feat: elevator to setpoint

---
+++ src/main/java/frc/robot/commands/elevator/ElevatorToSetpoint.java
@@ -0,0 +1,48 @@
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+public class ElevatorToSetpoint extends Command {
+    private final PIDController m_Controller;
+    private final Elevator elevator;
+    
+    public ElevatorToSetpoint(Elevator elevator) {
+        this.elevator = elevator;
+        m_Controller = new PIDController(0.01, 0, 0.001);
+        m_Controller.setTolerance(0.02);
+        m_Controller.setSetpoint(0);
+        addRequirements(elevator);
+        // Use addRequirements() here to declare subsystem dependencies.
+        // Configure additional PID options by calling `getController` here.
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        // double value = Math.clamp(m_Controller.calculate(m_Intake.getPivotDegrees(), 45), minval, maxval);
+
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        double value = MathUtil.clamp(m_Controller.calculate(elevator.getHeight(), 1), 0.3, 1.6);
+        System.out.println("Angle: " + elevator.getHeight() + ", Value: " + value + ", AtPoint: " + m_Controller.atSetpoint());
+        elevator.set(value);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        elevator.stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return m_Controller.atSetpoint();
+    }
+    
+}
